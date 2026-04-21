7c6f2144ada6b4f9803e5e21e2f7b14d386aa473
Bernardo Carro Roldán <bernyj100@gmail.com>
2026-04-21 23:06:00 +0200

    Added tags for OpenAPI documentation in Video and User controllers

--- VideoMinerTemplate26-main/src/main/java/aiss/videominer/Controllers/UserController.java
+++ VideoMinerTemplate26-main/src/main/java/aiss/videominer/Controllers/UserController.java
@@ -3,6 +3,13 @@ package aiss.videominer.Controllers;
 import aiss.videominer.Repositories.UserRepository;
 import aiss.videominer.exception.UserNotFoundException;
 import aiss.videominer.model.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@Tag(name= "User",description = "User management API")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -25,14 +33,34 @@ public class UserController {
 
     // Operaciones
     //GET http://localhost:8080/api/users
+    @Operation(
+            summary = "Retrieve all users",
+            description = "List all users",
+            tags = {"users","get"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "Listado de usuarios",
+            content = {@Content(schema = @Schema(implementation = User.class),mediaType = "application/json")})
+    })
     @GetMapping
     public List<User> findAll(){
         return userRepository.findAll();
     }
 
     //GET http://localhost:8080/api/users/{id}
+    @Operation(
+            summary = "Retrieve one user",
+            description = "Find one user based on an Id",
+            tags = {"users","get"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "Listado de un usuario",
+                    content = {@Content(schema = @Schema(implementation = User.class),mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404",description = "Usuario no encontrado",
+                    content = {@Content(schema = @Schema())})
+    })
     @GetMapping("/{id}")
-    public User findOneById(@PathVariable long id) throws UserNotFoundException {
+        public User findOneById(@Parameter(description = "id del usuario a buscar") @PathVariable long id) throws UserNotFoundException {
         Optional<User> user = userRepository.findById(id);
 
         if(user.isEmpty()){
@@ -43,18 +71,43 @@ public class UserController {
     }
 
     //POST http://localhost:8080/api/users
+    @Operation(
+            summary = "Create a user",
+            description = "Post a user",
+            tags = {"users","post"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201",description = "Creación de un usuario",
+                    content = {@Content(schema = @Schema(implementation = User.class),mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400",description = "Usuario no creado",
+                    content = {@Content(schema = @Schema())})
+    })
     @ResponseStatus(HttpStatus.CREATED) // 201
     @PostMapping
-    public User create(@Valid @RequestBody User user){
+    public User create(@Parameter(description = "Cuerpo del usuario a crear") @Valid @RequestBody User user){
         User _user = userRepository.save(new User(user.getName(),user.getUser_link(),
                 user.getPicture_link()));
         return user;
     }
 
     //PUT http://localhost:8080/api/users/{id}
+    @Operation(
+            summary = "Update a user",
+            description = "Update a used based on an Id",
+            tags = {"users","put"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "204",description = "Actualización de un usuario",
+                    content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "400",description = "Usuario no actualizado",
+                    content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "404",description = "Usuario no encontrado",
+                    content = {@Content(schema = @Schema())})
+    })
     @ResponseStatus(HttpStatus.NO_CONTENT) // 204
     @PutMapping("/{id}")
-    public void update(@Valid @RequestBody User updatedUser,@PathVariable long id) throws
+    public void update(@Parameter(description = "Cuerpo del usuario a actualizar") @Valid @RequestBody User updatedUser,
+                       @Parameter(description = "id del usuario a actualizar") @PathVariable long id) throws
             UserNotFoundException{
         Optional<User> userData = userRepository.findById(id);
 
@@ -71,9 +124,22 @@ public class UserController {
     }
 
     //DELETE http://localhost:8080/api/users/{id}
+    @Operation(
+            summary = "Delete a user",
+            description = "Delete a user based on an Id",
+            tags = {"users","delete"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "204",description = "Borrado de un usuario",
+                    content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "400",description = "Usuario no borrado",
+                    content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "404",description = "Usuario no encontrado",
+                    content = {@Content(schema = @Schema())})
+    })
     @ResponseStatus(HttpStatus.NO_CONTENT) // 204
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable long id){
+    public void delete(@Parameter(description = "id del usuario a borrar") @PathVariable long id){
         if (userRepository.existsById(id)){
             userRepository.deleteById(id);
         }
--- VideoMinerTemplate26-main/src/main/java/aiss/videominer/Controllers/VideoController.java
+++ VideoMinerTemplate26-main/src/main/java/aiss/videominer/Controllers/VideoController.java
@@ -2,7 +2,15 @@ package aiss.videominer.Controllers;
 
 import aiss.videominer.Repositories.VideoRepository;
 import aiss.videominer.exception.VideoNotFoundException;
+import aiss.videominer.model.User;
 import aiss.videominer.model.Video;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@Tag(name = "Video",description = "Video management API")
 @RestController
 @RequestMapping("/api/videos")
 public class VideoController {
@@ -25,14 +34,34 @@ public class VideoController {
 
     // Operaciones
     //GET http://localhost:8080/api/users
+    @Operation(
+            summary = "Retrieve all videos",
+            description = "List all videos",
+            tags = {"videos","get"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "Listado de videos",
+                    content = {@Content(schema = @Schema(implementation = Video.class),mediaType = "application/json")})
+    })
     @GetMapping
     public List<Video> findAll(){
         return videoRepository.findAll();
     }
 
     //GET http://localhost:8080/api/videos/{id}
+    @Operation(
+            summary = "Retrieve one video",
+            description = "Obtain a video based on an Id",
+            tags = {"videos","get"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "Listado de un videos",
+                    content = {@Content(schema = @Schema(implementation = Video.class),mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404",description = "Video no encontrado",
+                    content = {@Content(schema = @Schema())})
+    })
     @GetMapping("/{id}")
-    public Video findOneById(@PathVariable long id) throws VideoNotFoundException {
+    public Video findOneById(@Parameter(description = "id del video a buscar") @PathVariable long id) throws VideoNotFoundException {
         Optional<Video> video = videoRepository.findById(id);
 
         if (video.isEmpty()){
@@ -43,9 +72,20 @@ public class VideoController {
     }
 
     //POST http://localhost:8080/api/users
+    @Operation(
+            summary = "Create a video",
+            description = "POST a video",
+            tags = {"videos","post"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201",description = "Creación de un Video",
+                    content = {@Content(schema = @Schema(implementation = Video.class),mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400",description = "Video no creado",
+                    content = {@Content(schema = @Schema())})
+    })
     @ResponseStatus(HttpStatus.CREATED) // 201
     @PostMapping
-    public Video create(@Valid @RequestBody Video video){
+    public Video create(@Parameter(description = "Cuerpo del video a crear") @Valid @RequestBody Video video){
         Video _video = videoRepository.save(new Video(video.getName(),
                 video.getDescription(),
                 video.getReleaseTime()));
@@ -54,9 +94,23 @@ public class VideoController {
     }
 
     //PUT http://localhost:8080/api/users/{id}
+    @Operation(
+            summary = "Update a video",
+            description = "Update a video based on an Id",
+            tags = {"videos","put"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "204",description = "Actualización de un Video",
+                    content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "400",description = "Video no actualizado",
+                    content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "404",description = "Video no encontrado",
+                    content = {@Content(schema = @Schema())})
+    })
     @ResponseStatus(HttpStatus.NO_CONTENT) // 204
     @PutMapping("/{id}")
-    public void update(@Valid @RequestBody Video updatedVideo,@PathVariable long id) throws VideoNotFoundException{
+    public void update(@Parameter(description = "Cuerpo del usuario a actualizar") @Valid @RequestBody Video updatedVideo,
+                       @Parameter(description = "id del video a actualizar") @PathVariable long id) throws VideoNotFoundException{
         Optional<Video> videoData = videoRepository.findById(id);
 
         if (videoData.isEmpty()){
@@ -71,9 +125,22 @@ public class VideoController {
     }
 
     //DELETE http://localhost:8080/api/users/{id}
+    @Operation(
+            summary = "Delete a video",
+            description = "Delete a video based on an Id",
+            tags = {"videos","delete"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "204",description = "Borrado de un video",
+                    content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "400",description = "Video no borrado",
+                    content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "404",description = "Video no encontrado",
+                    content = {@Content(schema = @Schema())})
+    })
     @ResponseStatus(HttpStatus.NO_CONTENT) // 204
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable long id) {
+    public void delete(@Parameter(description = "id del video a borrar") @PathVariable long id) {
         if (videoRepository.existsById(id)){
             videoRepository.deleteById(id);
         }
