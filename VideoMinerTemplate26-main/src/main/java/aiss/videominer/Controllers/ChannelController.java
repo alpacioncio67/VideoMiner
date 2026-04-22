bfcb5135d7d1f2b7f3ca148fa232a4bcc7f412c1
PabloVela06 <pablovelsan06@gmai.com>
2026-04-22 12:58:05 +0200

    Changes in routes (some words in plural)

--- VideoMinerTemplate26-main/src/main/java/aiss/videominer/Controllers/ChannelController.java
+++ VideoMinerTemplate26-main/src/main/java/aiss/videominer/Controllers/ChannelController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/videominer/channel")
+@RequestMapping("/videominer/channels")
 public class ChannelController {
 
     @Autowired
--- VideoMinerTemplate26-main/src/main/java/aiss/videominer/Controllers/CommentController.java
+++ VideoMinerTemplate26-main/src/main/java/aiss/videominer/Controllers/CommentController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/videominer/comment")
+@RequestMapping("/videominer/comments")
 public class CommentController {
 
     @Autowired
